61510f089854f4ea4add3617a229d08afba28d47
Conor O'Neill <cconeill1998@gmail.com>
2022-11-15 11:25:30 +0000

    fix merge issues

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById("parse-btn").addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            files: ['downloadTableData.js'],
+         });
+    });
+});
